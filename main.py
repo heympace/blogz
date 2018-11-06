5a812d49f00b84529918a14f7ce8ab29e8cbade2
MPace <heympace2@gmail.com>
2018-11-05 20:00:32 -0600

    finalize error messages, add index with list of users

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class User(db.Model):
 def require_login():
 
     # need to whitelist pages user can see
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['login', 'list_users', 'list_blog', 'index', 'signup']
 
     # if endpoint isn't in allowed routes and no email...
     if request.endpoint not in allowed_routes and 'username' not in session:
@@ -51,6 +51,39 @@ def require_login():
 def empty_string(response):
     if response == "":
         return True
+
+# check for 3+ characters
+def under_three_chars(response):
+    if len(response) < 3:
+        return True
+
+
+#########################
+###     index
+#########################
+
+@app.route('/', methods=['POST', 'GET'])
+def list_users():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+    # get first, assign to variable, 
+    # user_id = request.args.get('id') 
+    # members = User.query.filter_by(username=username).all()
+
+    #if user_id is None:
+    #    users = User.query.filter_by(id=id).all()
+
+        # archived = Blog.query.filter_by(completed=True).all()
+    
+    #    return render_template('index.html',title="User List", 
+    #        users=users)
+    #else:
+    #    user = Blog.query.get(user_id)
+    #    return render_template('post.html', 
+    #        post_id=post_id, 
+    #        post_title=post.title, 
+    #        post_body=post.body
+    #    )
 
 
 #########################
@@ -74,9 +107,12 @@ def login():
             # remember that the user is logged in
             session['username'] = username
             flash("Logged in")
-            return redirect ('/blog')
-        else:
-            flash("User password incorrect, or user does not exist.", "error")
+            return redirect ('/newpost')
+
+        elif user and not user.password == password:
+            flash("User password incorrect, please try again", "error")
+        elif not user:
+            flash("Username does not exist, please try again", "error")
 
     return render_template('login.html')
 
@@ -93,11 +129,33 @@ def signup():
         password = request.form['password']
         verify = request.form['password_verify']
 
-        # TODO - validate user's data
-
         # test for existing user
         existing_user = User.query.filter_by(username=username).first()
-        
+
+        if existing_user:
+            flash("Username already exists, please enter a new username", "error")
+            return render_template('signup.html')
+
+        if empty_string(username):
+            flash("Please enter a username", "error")
+            return render_template('signup.html')
+
+        if under_three_chars(username):
+            flash("Username must be 3 or more characters", "error")
+            return render_template('signup.html')
+
+        if empty_string(password):
+            flash("Please enter a password", "error")
+            return render_template('signup.html', username=username)
+
+        if under_three_chars(password):
+            flash("Password must be 3 or more characters", "error")
+            return render_template('signup.html')
+
+        if password != verify:
+            flash("Passwords don't match, please enter password", "error")
+            return render_template('signup.html', username=username)
+
         if not existing_user:
             # add new user to db
             new_user = User(username, password)
@@ -107,11 +165,11 @@ def signup():
             # remember that the user is logged in
             session['username'] = username
 
-            return redirect ('/blog')
+            return redirect ('/newpost')
         
         else:
             # TODO - user better response message
-            return "<h1>User already exists</h1>"
+            return "<h1>Please try a different username.</h1>"
 
     return render_template('signup.html')
 
@@ -121,12 +179,13 @@ def signup():
 #########################
 
 @app.route('/blog', methods=['POST', 'GET'])
-def index():
+def list_blogs():
     # get first, assign to variable, 
     post_id = request.args.get('id') 
+    owner = User.query.filter_by(username=session['username']).first()
 
     if post_id is None:
-        posts = Blog.query.filter_by(archived=False).all()
+        posts = Blog.query.filter_by(archived=False, owner=owner).all()
         # archived = Blog.query.filter_by(completed=True).all()
     
         return render_template('blog.html',title="My Blog", 
@@ -158,13 +217,15 @@ def addPost():
         error_post_body = ''
 
         if empty_string(post_title):
-            error_post_title = 'Title required'
+            flash("Please enter a post title", "error")
+            return render_template('newpost.html', post_body=post_body)
 
         if empty_string(post_body):
-            error_post_body = 'Content required'
+            flash("Please add post body content", "error")
+            return render_template('newpost.html', post_title=post_title)
 
         if not error_post_title and not error_post_body:
-            new_post = Blog(post_title, post_body, archived)
+            new_post = Blog(post_title, post_body, archived, owner)
 
             db.session.add(new_post)
             db.session.commit()
@@ -176,8 +237,7 @@ def addPost():
             return render_template('/newpost.html',
                 post_title=post_title,
                 post_body=post_body,
-                error_post_title=error_post_title,
-                error_post_body=error_post_body)
+                )
 
     posts = Blog.query.filter_by(archived=False).all()
     # archived = Blog.query.filter_by(completed=True).all()
@@ -192,7 +252,7 @@ def addPost():
 @app.route('/logout')
 def logout():
     del session['username']
-    return redirect('/')
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
